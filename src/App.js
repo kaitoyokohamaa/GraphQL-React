d3d958082c812123f6269c7ad237d7b29bd5e068
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-10-03 18:38:00 +0900

    apply query and variables

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {ApolloProvider} from "react-apollo"
 import client from "./client"
 import { Query } from "react-apollo"
-import {ME} from "./graphql"
+import {SEARCH_REPOSITORIES} from "./graphql"
+const VARIABLES = {
+  first:5,
+  after:null,
+  last:null,
+  before:null,
+  query:"フロントエンドエンジニア"
+}
 function App() {
+  const [variable,setValiables]=useState(VARIABLES)
+  const {query, first, last, before, after}=variable
+  console.log(setValiables)
   return (
     <ApolloProvider client={client}>
       <div>hello, graphQL</div>
-      <Query query={ME}>
+      <Query 
+        query={SEARCH_REPOSITORIES}
+        variables={{query, first, last, before, after}}
+      >
         {
           ({ loading, error, data})=>{
             if(loading) return "loading..."
             if(error) return `Error ${error.message}`
-            console.log(data.user.name)
-            return <div>{data.user.name}</div>
+            console.log(data)
+            return <div>{}</div>
           }
         }
       </Query>
